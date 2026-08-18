13c5b258dabc7e93c6f80f0300d7071f6b590b9c
peter-njuku <njuku2005@gmail.com>
2026-08-18 15:19:01 +0300

    Request context logging

--- middleware.go
+++ middleware.go
@@ -1,15 +1,62 @@
 package main
 
 import (
+	"io"
 	"log/slog"
 	"net"
 	"net/http"
+	"time"
 )
+
+type spyReadCloser struct {
+	io.ReadCloser
+	bytesRead int
+}
+
+type spyResponseWriter struct {
+	http.ResponseWriter
+	bytesWritten int
+	statusCode   int
+}
+
+func (r *spyReadCloser) Read(p []byte) (int, error) {
+	if r.ReadCloser == nil {
+		return 0, io.EOF
+	}
+	n, err := r.ReadCloser.Read(p)
+	r.bytesRead += n
+	slog.Info("Body Read", "bytes", n, "err", err)
+	return n, err
+}
+
+func (w *spyResponseWriter) Write(p []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+
+	n, err := w.ResponseWriter.Write(p)
+	w.bytesWritten += n
+	return n, err
+}
+
+func (w *spyResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
 
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
+			start := time.Now()
+
+			spyReader := &spyReadCloser{ReadCloser: r.Body}
+			spyWriter := &spyResponseWriter{
+				ResponseWriter: w,
+				statusCode:     http.StatusOK,
+			}
+
+			r.Body = spyReader
+			next.ServeHTTP(spyWriter, r)
 
 			clientIP := r.RemoteAddr
 			if host, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
@@ -21,6 +68,10 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("client_ip", clientIP),
+				slog.Duration("duration", time.Since(start)),
+				slog.Int("request_body_bytes", spyReader.bytesRead),
+				slog.Int("response_status", spyWriter.statusCode),
+				slog.Int("response_body_bytes", spyWriter.bytesWritten),
 			)
 		})
 	}
